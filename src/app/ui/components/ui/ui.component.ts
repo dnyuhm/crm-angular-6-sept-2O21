f2a0aa5553fbbfe82af1117e562ba6c06784c3e9
dylanmeuriot <dylanmeuriot@gmail.com>
2021-09-06 17:37:05 +0200

    Commit 10 : NgIf et ng-template

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UiComponent implements OnInit {
   public open: boolean;
   constructor() {
-    this.open = true;
+    this.open = false;
   }
 
   ngOnInit(): void {}
