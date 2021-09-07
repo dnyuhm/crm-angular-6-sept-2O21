0b58eadf361d624e6960a0fb67556370f815ad20
dylanmeuriot <dylanmeuriot@gmail.com>
2021-09-07 15:56:27 +0200

    Commit 22 : Templates de page avec un title statique

--- src/app/templates/templates.module.ts
+++ src/app/templates/templates.module.ts
@@ -1,8 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TemplateFullWidthComponent } from './components/template-full-width/template-full-width.component';
+import { TemplateContainerComponent } from './components/template-container/template-container.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TemplateFullWidthComponent,
+    TemplateContainerComponent
+  ],
   imports: [CommonModule],
+  exports: [
+    TemplateFullWidthComponent,
+    TemplateContainerComponent
+  ],
 })
 export class TemplatesModule {}
