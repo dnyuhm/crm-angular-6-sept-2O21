74d20e3136898232365fdb4c6e55665f715f946e
dylanmeuriot <dylanmeuriot@gmail.com>
2021-09-07 09:56:02 +0200

    Commit 12 : ui2 & agilité

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoreModule } from '../core/core.module';
 import { OrdersRoutingModule } from './orders-routing.module';
-import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 
 @NgModule({
   declarations: [
     PageListOrdersComponent,
     PageAddOrderComponent,
-    PageEditOrderComponent
+    PageEditOrderComponent,
   ],
   imports: [CommonModule, OrdersRoutingModule],
 })
